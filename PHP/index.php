c14143933367d0f1d2f022fb86d5da600bdc522d
S M Rifat Mostakim <smrifatmostakim68@gmail.com>
2026-01-03 21:27:33 +0600

    add php file

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'one punch man';
+$a = 10;
+$b = 20;
+$c = $a + $b + 20;
+echo $c;
+define('one', 3);
+echo one + 3;
+printf("name=%s, Age=%d", $name, $a);
+$age=20;
+
+$msg="...";
+$age>=18?($msg="Your are adult"):($msg="You are boy");
+echo $msg;
+
+
